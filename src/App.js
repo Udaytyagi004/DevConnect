ecb048e56a642d648ec11a2d6de8656858964e6b
Uday Narayan Tyagi <tyagiuday1410@gmail.com>
2025-11-10 23:53:31 +0530

    make CRUD APIs

--- src/App.js
+++ src/App.js
@@ -2,21 +2,94 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { Collection } = require("mongoose");
 
+// using MiddleWare (provided by express) for reading the json data of requests ==> this wil work for every request on this server
+app.use(express.json());
 app.post("/signUp", async (req, res) => {
-  const userData = {
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "virat123@gmail.com",
-    password: "virat@000",
-    age: 35,
-    gender: "Male",
-  };
   // creating the new instance of the User Model
-  const user = new User(userData);
-  await user.save();
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user is saved successfully");
+  } catch (err) {
+    res.status(400).send("there is an Error while sign up");
+  }
+});
 
-  res.send("user is saved successfully");
+// getting a user with the emailID
+
+app.get("/user", async (req, res) => {
+  try {
+    const email = req.body.emailId;
+    const userData = await User.findOne({ emailId: email });
+    if (userData.length === 0) {
+      res.status(404).send("User Not Found");
+    }
+    console.log(typeof userData);
+    res.send(userData);
+  } catch (err) {
+    res.status(400).send("Something went Wrong");
+  }
+});
+
+// get the user with an Id
+app.get("/getbyID", async (req, res) => {
+  const id = "690f407274a682aff36ed637";
+  try {
+    const user = await User.findById(id);
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+// Feed API => get the data of all the users
+app.get("/feed", async (req, res) => {
+  try {
+    const userData = await User.find({});
+    if (userData.length === 0) {
+      res.status(404).send("User Not Found");
+    }
+    res.send(userData);
+  } catch (err) {
+    res.status(400).send("Something went Wrong");
+  }
+});
+
+// delete API
+app.delete("/user", async (req, res) => {
+  const id = req.body.Id;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).send("user is not found");
+    }
+    const username = user.firstName;
+
+    await User.findByIdAndDelete(id);
+    res.send(`delete the data of ${username} successfully`);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//update API
+app.patch("/user", async (req, res) => {
+  const id = req.body.Id;
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      {
+        firstName: "rohit",
+        age: [23],
+      },
+      { new: true, strict: false }
+    );
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 connectDB()
